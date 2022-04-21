331863d6add39c0853819512973189a3baeea3fb
SteveTheEngineer <sthesengineer@gmail.com>
2022-04-21 21:23:32 +0300

    GitHub Workflows attempt 1.

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,3 +24,21 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.yaml:snakeyaml:1.30")
 }
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/SteveTheEngineer/SS-BukkitGradle")
+            credentials {
+                username = (project.findProperty("gpr.user") ?: System.getenv("USERNAME")).toString()
+                password = (project.findProperty("gpr.key") ?: System.getenv("TOKEN")).toString()
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("gpr") {
+            from(components.getByName("java"))
+        }
+    }
+}
